c645602bf89de0b3530c32fd017eea2e744d5fc6
saqebkhan <saqebk619@gmail.com>
2022-03-10 21:27:23 +0530

    saved and pushed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import LogInPage from './components/Login Page/LogInPage'
 
 const App = () => {
+
   return (
     <div>
       <LogInPage />
-    </div>
+      </div>
   );
 };
 
